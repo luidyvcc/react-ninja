d840195d93dc93d6abdf10ec4bde0ee04ef064f0
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-02-04 07:28:23 -0300

    m1#a24 done

--- 003-introducao-parte-2/src/app.js
+++ 003-introducao-parte-2/src/app.js
@@ -1,29 +1,31 @@
 'use strict'
 import React, { Component } from 'react'
-import Button from './button'
-import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
   constructor () {
+    console.log('constructor')
     super()
     this.state = {
-      color: 'green'
+      time: 0,
+      showTimer: true
     }
   }
 
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+
   render () {
+    console.log('render')
     return (
       <div>
-        <Square color={this.state.color}/>
-
-        {['red', 'green', 'blue'].map(color => (
-          <Button
-            key={color}
-            handleClick={() => this.setState({ color })}
-          >
-            {color}
-          </Button>
-        ))}
+        <Timer time={this.state.time} />
+        <button onClick={() => this.setState({ time: this.state.time + 10 })}>Change props</button>
       </div>
     )
   }
---
+++ 003-introducao-parte-2/src/timer.js
@@ -0,0 +1,36 @@
+'use strict'
+import React, { Component } from 'react'
+
+class Timer extends Component {
+  constructor () {
+    super()
+    this.state = {
+      time: 0
+    }
+    this.timer
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps: ', this.props, nextProps)
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => this.setState({ time: this.state.time + 1 }), 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate: ', nextProps, nextState)
+    return this.props.time !== nextProps.time
+  }
+
+  render () {
+    console.log('render timer')
+    return (<div>Timer: {this.state.time}</div>)
+  }
+}
+
+export default Timer
