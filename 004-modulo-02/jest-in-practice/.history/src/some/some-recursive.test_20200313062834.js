e73ffa180e3f0a208b9704f7cddcc651befd7fb5
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-03-13 06:33:52 -0300

    m2#a19 done

---
+++ 004-modulo-02/jest-in-practice/.history/src/some/some-recursive.test_20200313062834.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import { expect } from 'chai'
+import some from './some'
+
+it('some should be a function', () => {
+  expect(some).to.be.a('function');
+})
+
+it('some([], item => item', () => {
+  expect(some([], item => item)).to.not.be.ok; // boolean test
+})
+
+it('some([1, 2], item => item', () => {
+  expect(some([1, 2], item => item)).to.be.ok; // boolean test
+})
+
+it('some([1, 5, 3], (_item, index) => index % 2 === 0 should return true', () => {
+  expect(some([1, 5, 3], (_item, index) => index % 2 === 0)).to.be.ok; // boolean test
+})
+
+it('some([1, 5, 3], item => item % 2 === 0 should return false', () => {
+  expect(some([1, 5, 3], item => item % 2 === 0)).to.not.be.ok; // boolean test
+})
+
+it('some([1, 2, 3], (_item, _index, array) => array.indexOf(2) >= 0) should return true', () => {
+  expect(some([1, 2, 3], (_item, _index, array) => array.indexOf(2) >= 0)).to.be.ok; // boolean test
+})
+
+it('some([1, 2, 3], item => item > 2', () => {
+  expect(some([1, 2, 3], item => item > 2)).to.be.ok; // boolean test
+})
+
+it('some([1, 2, 3], item => item > 3', () => {
+  expect(some([1, 2, 3], item => item > 3)).to.not.be.ok; // boolean test
+})
---
+++ 004-modulo-02/jest-in-practice/.history/src/some/some-recursive_20200313062828.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const every = (arr, func) => {
+  return (function everyInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
+    return arrayInternal.length === 0
+      ? false : func(head, counter, arr)
+        ? true : everyInternal(tail, counter + 1)
+  })(arr, 0)
+}
+export default every
