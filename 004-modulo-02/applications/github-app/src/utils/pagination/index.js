f8f8677a8dfb20700f44295e2a632eda1bf51376
Luidy vaccaro <luidy@upoutsourcing.com>
2020-04-30 07:04:11 -0300

    m2#a53 done

--- 004-modulo-02/applications/github-app/src/utils/pagination/index.js
+++ 004-modulo-02/applications/github-app/src/utils/pagination/index.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const pagination = ({ total, activePage }) => {
-  return Array.from({ length: total }, (_, i) => i + 1)
+  if (total <= 5) {
+    return Array.from({ length: total }, (_, i) => i + 1)
+  }
+  return [1, 2, 3, '...', 6];
 }
 
 export default pagination
