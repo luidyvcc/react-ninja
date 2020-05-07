86c297503b5730578db33579360896e3916cedc1
Luidy vaccaro <luidy@upoutsourcing.com>
2020-05-07 07:02:28 -0300

    m2#a57 done

--- 004-modulo-02/applications/github-app/src/utils/pagination/index.js
+++ 004-modulo-02/applications/github-app/src/utils/pagination/index.js
@@ -1,18 +1,24 @@
 'use strict'
 
-const centerRule = ({ total, activePage }) => {
-  if (activePage - 1 <= 0) {
-    return 1
+const centerRule = ({ total, activePage }) => (
+  activePage - 1 <= 0
+    ? 1
+    : activePage === total
+      ? activePage - 2
+      : activePage - 1
+)
+
+const isNumber = (value) => typeof value === 'number'
+
+const pagination = ({ total = 1, activePage = 1 } = {}) => {
+  if (!isNumber(total)) {
+    throw new TypeError('total should be a number')
   }
 
-  if (activePage === total) {
-    return activePage - 2
+  if (!isNumber(activePage)) {
+    throw new TypeError('activePage should be a number')
   }
 
-  return activePage - 1
-}
-
-const pagination = ({ total, activePage }) => {
   if (total <= 5) {
     return Array.from({ length: total }, (_, i) => i + 1)
   }
@@ -27,13 +33,13 @@ const pagination = ({ total, activePage }) => {
   pages = pages.filter((page, index, array) => array.indexOf(page) === index)
 
   let firstPage = pages[0]
-  let scoundPage = pages[1]
+  let secondPage = pages[1]
 
-  if (scoundPage === (firstPage + 2)) {
+  if (secondPage === (firstPage + 2)) {
     pages = [
       firstPage,
       firstPage + 1,
-      ...pages.slice(1),
+      ...pages.slice(1)
     ]
   }
 
@@ -49,13 +55,13 @@ const pagination = ({ total, activePage }) => {
   }
 
   firstPage = pages[0]
-  scoundPage = pages[1]
+  secondPage = pages[1]
 
-  if (scoundPage > (firstPage + 2)) {
+  if (secondPage > (firstPage + 2)) {
     pages = [
       firstPage,
-      "...",
-      ...pages.slice(1),
+      '...',
+      ...pages.slice(1)
     ]
   }
 
