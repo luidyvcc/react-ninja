7d2654f3a575fe7064760640c4fbc6b3548095e0
Luidy vaccaro <luidy@upoutsourcing.com>
2020-05-11 07:46:12 -0300

    m2#a59 done

--- 004-modulo-02/applications/github-app/src/components/pagination/pagination.story.js
+++ 004-modulo-02/applications/github-app/src/components/pagination/pagination.story.js
@@ -7,6 +7,6 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
-  <Pagination total={10} />
+  <Pagination total={10} pageLink='http://mypage.com/page/%page%' />
 ))
 
