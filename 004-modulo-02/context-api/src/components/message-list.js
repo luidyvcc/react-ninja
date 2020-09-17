83b5218d491cf63a085bf193b7a6332f0bed1551
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-09-17 06:37:39 -0300

    m2#a136 done

--- 004-modulo-02/context-api/src/components/message-list.js
+++ 004-modulo-02/context-api/src/components/message-list.js
@@ -1,35 +1,14 @@
 'use strict'
 
-import React, { Component } from 'react'
-import { PropTypes } from 'prop-types'
+import React from 'react'
 import Message from './message'
 
-class MessageList extends Component {
-  componentDidMount () {
-    this.context.store.subscribe(() => this.forceUpdate())
-  }
-
-  shouldComponentUpdate () {
-    return false
-  }
-
-  render () {
-    return (
-      <ul>
-        {this.props.messages.map((message) => (
-          <Message
-            text={message.text}
-            color={message.color}
-            key={message.text}
-          />
-        ))}
-      </ul>
-    )
-  }
-}
-
-MessageList.contextTypes = {
-  store: PropTypes.object
-}
+const MessageList = ({ messages }) => (
+  <ul>
+    {messages.map((message) => (
+      <Message text={message.text} color={message.color} key={message.text} />
+    ))}
+  </ul>
+)
 
 export default MessageList
--- 004-modulo-02/context-api/src/components/message.js
+++ 004-modulo-02/context-api/src/components/message.js
@@ -1,29 +1,13 @@
 'use strict'
 
-import React, { Component } from 'react'
-import { PropTypes } from 'prop-types'
-import after3s from './after3s'
+import React from 'react'
+import withContext from './with-context'
 
-class Message extends Component {
-  componentDidMount () {
-    this.unsubscribe = this.context.store.subscribe(() => this.forceUpdate())
-  }
+const Message = ({ store, text, color}) => (
+  <li style={{ background: store.color }}>
+    {text}
+    <button onClick={store.setColor(color)}>Change Color</button>
+  </li>
+)
 
-  componentWillUnmount () {
-    this.unsubscribe()
-  }
-  render () {
-    return (
-      <li style={{ background: this.context.store.color }}>
-        {this.props.text}
-        <button onClick={this.context.store.setColor(this.props.color, () => this.forceUpdate())}>Change Color</button>
-      </li>
-    )
-  }
-}
-
-Message.contextTypes = {
-  store: PropTypes.object
-}
-
-export default after3s(Message)
+export default withContext(Message)
