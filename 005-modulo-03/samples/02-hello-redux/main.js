b4e01f8f5188515456b249d2d8b61be3a454b377
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-10-01 07:25:21 -0300

    m3#a09 done

---
+++ 005-modulo-03/samples/02-hello-redux/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+    default: return state
+  }
+}
+
+const { createStore } = Redux
+const store = createStore(counter)
+
+store.subscribe(() => {
+  console.log('Disparou uma ação! ', store.getState())
+})
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
