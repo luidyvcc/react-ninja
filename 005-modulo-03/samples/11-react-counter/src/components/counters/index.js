90ff1dbb527f8e8dfc41d4e965e3ae053f045bee
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-10-14 06:52:22 -0300

    m3#a22 done

---
+++ 005-modulo-03/samples/11-react-counter/src/components/counters/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import React from 'react'
+import { connect } from 'react-redux'
+import Counter from 'components/counter'
+import { ADD_COUNTER, REMOVE_COUNTER, INCREMENT, DECREMENT } from 'reducers/counters'
+
+const Counters = ({ counters, addCounter, removeCounter, increment, decrement }) => (
+  <div>
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      {counters.map((counter, index) => (
+        <Counter {...{
+          key: index,
+          counter,
+          removeCounter: removeCounter(index),
+          increment: increment(index),
+          decrement: decrement(index)
+        }} />
+      ))}
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '10px' }}>
+        <button onClick={addCounter}>Adicionar contador</button>
+    </div>
+  </div>
+)
+
+const mapStateToProps = (state) => ({
+  counters: state
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  addCounter: () => dispatch({ type: ADD_COUNTER }),
+  removeCounter: (index) => () => dispatch({ type: REMOVE_COUNTER, index }),
+  increment: (index) => () => dispatch({ type: INCREMENT, index }),
+  decrement: (index) => () => dispatch({ type: DECREMENT, index })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counters)
