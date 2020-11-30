d48618532f8ba8e573248cc13a7bc31f3f7df059
Luidy Vaccaro <luidy@upoutsourcing.com>
2020-11-30 19:50:26 -0300

    m3#a54 done

---
+++ 005-modulo-03/samples/35-todo-list-get-cep/src/components/search-cep/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+import SearchCep from './search-cep'
+import ajax from '@fdaciuk/ajax'
+
+class SearchCepContainer extends PureComponent {
+  state = {
+    address: '',
+    city: '',
+    code: '',
+    district: '',
+    state: '',
+    status: 1,
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    const cep = e.target.cep.value
+    const response = await ajax.get(`https://ws.apicep.com/cep/${cep}.json`)
+    this.setState(response)
+  }
+
+  render () {
+    return (
+      <SearchCep {...this.state} handleSubmit={this.handleSubmit} />
+    )
+  }
+}
+
+export default SearchCepContainer
