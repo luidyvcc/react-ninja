caf633b7b9cfce397aa8b7ddd5a020ab92de860f
Luidy Vaccaro <luidy@upoutsourcing.com>
2021-01-15 07:40:07 -0300

    m3#a78 done

---
+++ 005-modulo-03/samples/49-reactflix/06/src/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { Provider } from 'react-redux'
+import App from './app'
+import configureStore from './redux-flow/configure-store'
+
+const store = configureStore()
+
+const db = firebase.database()
+const videos = db.ref('videos')
+
+// videos.on('value', snapshot => {
+//   console.log('snapshot: ', snapshot.val())
+// }, error => {
+//   console.log('error: ', error)
+// })
+
+videos.once('value').then(snapshot => {
+  console.log('snapshot: ', snapshot.val())
+})
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
